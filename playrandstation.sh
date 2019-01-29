dd44cb845eacf0d1e4805e900be3d8015ab3799e
vague <iwhall42@students.tntech.edu>
2019-01-29 16:00:58 -0600

    Update the script to match radcap.ru changing page layout ( They removed playlist files )

--- playrandstation.sh
+++ playrandstation.sh
@@ -2,13 +2,13 @@
 # vague
 # pick a random radcap.ru station and play it
 
-prefix='http://radcap.ru/'
-
+# 'q' to quit mpv and load next random url
 while true ; do
-	url=$prefix$(shuf -n 1 <(wget -q -O - http://radcap.ru/index-d.html | grep \"[a-z]*.html -o | cut -d '"' -f 2 ))
+	# get the url for a random radio station
+	url=http://radcap.ru/$(shuf -n 1 <(wget -q -O - http://radcap.ru/index-d.html | grep \"[a-z]*.html -o | cut -d '"' -f 2 ))
 	echo $url
-	m3u=$(wget -q -O - $url | grep play/rc[0-9]/radio+caprice-[-+a-z]*.m3u -o)
-	echo $prefix$m3u
-	mpv $prefix$m3u
+	# get the ip address and port to the streaming server
+	ip=$(wget -q -O - $url | grep src=\"http://[0-9\.:]*/ -o | sed -e 's/.*\"//')
+	mpv $ip
 done
 
