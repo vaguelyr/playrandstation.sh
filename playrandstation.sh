b8f87731c9cd56258b26595a32b60b8580bbdcbb
vague <iwhall42@students.tntech.edu>
2019-02-01 12:24:01 -0600

    improved the output for saving favorite streams

--- playrandstation.sh
+++ playrandstation.sh
@@ -6,9 +6,14 @@
 while true ; do
 	# get the url for a random radio station
 	url=http://radcap.ru/$(shuf -n 1 <(wget -q -O - http://radcap.ru/index-d.html | grep \"[a-z]*.html -o | cut -d '"' -f 2 ))
-	echo $url
 	# get the ip address and port to the streaming server
 	ip=$(wget -q -O - $url | grep src=\"http://[0-9\.:]*/ -o | sed -e 's/.*\"//')
-	mpv $ip
+	# output nicely so you can copy and paste and save your favorite stations
+	echo
+	echo \# $url
+	echo $ip
+	echo
+
+	mpv $ip --volume=70
 done
 
