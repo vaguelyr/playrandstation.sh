6a5952632a50d687c1e559586b0a632753bb6f53
vague <iwhall42@students.tntech.edu>
2019-02-09 16:12:47 -0600

    Added somafm

--- playrandstation.sh
+++ playrandstation.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # vague
-# pick a random radcap.ru station and play it
+# play random station
 
-# 'q' to quit mpv and load next random url
+
 while true ; do
-	# get the url for a random radio station
-	url=http://radcap.ru/$(shuf -n 1 <(wget -q -O - http://radcap.ru/index-d.html | grep \"[a-z]*.html -o | cut -d '"' -f 2 ))
-	# get the ip address and port to the streaming server
-	ip=$(wget -q -O - $url | grep src=\"http://[0-9\.:]*/ -o | sed -e 's/.*\"//')
-	# output nicely so you can copy and paste and save your favorite stations
-	echo
-	echo \# $url
-	echo $ip
-	echo
+	if [ "$(( RANDOM % 1000))" -ge "500" ] ; then
+		echo radcap
+		rand_station=http://radcap.ru/$(shuf -n 1 <(wget -q -O - http://radcap.ru/index-d.html | grep \"[a-z]*.html -o | cut -d '"' -f 2 ))
+		stream_url=$(wget -q -O - $rand_station | grep src=\"http://[0-9\.:]*/ -o | sed -e 's/.*\"//')
+	else
+		echo soma		
+		#rand_station=http://somafm.com/$(shuf -n1 <(wget http://somafm.com/ -q -O -  | grep cbshort -A1 | grep / | cut -d '/' -f2) )
+		#stream_url=http://somafm.com$(wget -q -O - $rand_station | grep ".pls" -m1 | cut -d '"' -f 2)
 
-	mpv $ip --volume=70
+		# faster, single wget
+		rand_station=http://somafm.com$(shuf -n 1 <(wget http://somafm.com/listen/ -O - | grep AAC.*.pls | cut -d '"' -f 2))
+		stream_url=$rand_station
+	fi
+	
+	echo
+	echo \# $rand_station
+	echo $stream_url
+	echo
+	
+	mpv $stream_url 
 done
-
