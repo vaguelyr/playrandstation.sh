bfd70d06aba22517d02e4f870565ba82cb273fae
vaguelyr <greymoth90@gmail.com>
2019-02-20 10:44:22 -0600

    improved the radcap scraper

--- playrandstation.sh
+++ playrandstation.sh
@@ -6,7 +6,7 @@
 while true ; do
 	if [ "$(( RANDOM % 1000))" -ge "500" ] ; then
 		echo radcap
-		rand_station=http://radcap.ru/$(shuf -n1 <(wget -q -O - http://radcap.ru/select.js | cut -d '"' -f 2 | grep html))
+		rand_station=http://radcap.ru/$(shuf -n1 <(wget -q -O - http://radcap.ru/select.js | grep '[^"]*.html' -o))
 		stream_url=$(wget -q -O - $rand_station | grep src=\"http://[0-9\.:]*/ -o | sed -e 's/.*\"//')
 	else
 		echo soma		
